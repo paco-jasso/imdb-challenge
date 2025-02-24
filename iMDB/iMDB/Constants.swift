6a3ed1f7b740b354cb13c125ab795a1138f429fb
Francisco Jasso <fcojassordz@gmail.com>
2025-02-23 18:56:12 -0600

    Created Constants file
    Adding Constants file with API key elements and URLs (sensitive data is omitted)

---
+++ iMDB/iMDB/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  iMDB
+//
+//  Created by Francisco Jasso on 23/02/25.
+//
+
+import Foundation
+
+class Constants {
+    //Do not share this values to the public. Avoid pushing to repo.
+    static let APIKey = ""
+    static let authToken = ""
+    
+    //This is safe
+    static let popularMoviesURL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+}
