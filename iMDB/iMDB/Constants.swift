ce5087d36124b8287dced29e5b15ac8001122e2d
Francisco Jasso <fcojassordz@gmail.com>
2025-02-23 23:52:39 -0600

    Larger poster image

--- iMDB/iMDB/Constants.swift
+++ iMDB/iMDB/Constants.swift
@@ -9,8 +9,8 @@ import Foundation
 
 class Constants {
     //Do not share this values to the public. Avoid pushing to repo.
-    static let APIKey = "c663bf22155fc95f909eed318db106f9"
-    static let authToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjYzYmYyMjE1NWZjOTVmOTA5ZWVkMzE4ZGIxMDZmOSIsIm5iZiI6MTc0MDM0NjI3My42NzcsInN1YiI6IjY3YmI5M2ExYTIzOTI5YWMyOGJlZjE0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LZJdsciPllY_3f9RlPrBotaeTLkwOFJheSvRJvqumEQ"
+    static let APIKey = "*****"
+    static let authToken = "*****"
     
     //This is safe
     static let popularMoviesURL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
