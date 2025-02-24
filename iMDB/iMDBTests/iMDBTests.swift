bb179bde72dcba59a6827aa3f93b3e734f8a513e
Francisco Jasso <fcojassordz@gmail.com>
2025-02-23 18:51:09 -0600

    Initial commit
    Project creation commit

---
+++ iMDB/iMDBTests/iMDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  iMDBTests.swift
+//  iMDBTests
+//
+//  Created by Francisco Jasso on 23/02/25.
+//
+
+import Testing
+@testable import iMDB
+
+struct iMDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
