8cbab179920313753f4effcbe8eb2894a62c1edd
Francisco Jasso <fcojassordz@gmail.com>
2025-02-23 19:57:39 -0600

    Extension to UIImageView
    This extension has a method to load images from URLs

---
+++ iMDB/iMDB/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIImageView+Extension.swift
+//  iMDB
+//
+//  Created by Francisco Jasso on 23/02/25.
+//
+
+import UIKit
+
+extension UIImageView {
+    func getPoster(from path: String) {
+        //This function gets the image from the URL
+        guard let url = URL(string: "\(Constants.postersURL)\(path)") else {
+            return
+        }
+        
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url), let loadedImage = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.image = loadedImage
+                }
+            }
+        }
+    }
+}
